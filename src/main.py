2c3bca7aecbf73d76de1c6c1f06de9cf7c58e268
Caidan Williams <caidanstevenwilliams@gmail.com>
2020-05-23 23:27:26 -0400

    Add deps and first iteration on git log scraping

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import io
+import re
+
+from sh import git
+
+
+class Commit:
+    RE_COMMIT_SHA = re.compile(r'^(.{7})\s(.+)$')
+
+    def __init__(self, log_line: str):
+        match = self.RE_COMMIT_SHA.match(log_line)
+        if match:
+            self.sha = match.group(1)
+            self.message = match.group(2)
+        else:
+            raise ValueError('Invalid log line: %s', log_line)
+
+
+if __name__ == '__main__':
+    buf = io.StringIO()
+    git.log('master..', '--oneline', '--no-decorate', _out=buf, _tty_out=False)
+    print(buf.getvalue())
+
+    branch_commits = buf.getvalue().splitlines()
+    for idx, ll in enumerate(branch_commits):
+        commit = Commit(ll)
+        print(idx, commit.sha, commit.message)
